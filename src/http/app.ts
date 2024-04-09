0ed9ba96c06e8a1314fce667b3ba9020f6e04307
Mayke Silva <dev.maykesilva@gmail.com>
2024-04-08 22:52:39 -0300

    feat: improving error handling

--- src/http/app.ts
+++ src/http/app.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import fastifyJwt from '@fastify/jwt'
+import { ZodError } from 'zod'
 import { env } from '@/env'
 import { usersRoutes } from './controllers/users/routes'
 import { gymsRoutes } from './controllers/gyms/routes'
@@ -12,3 +13,20 @@ app.register(fastifyJwt, { secret: env.JWT_SECRET })
 app.register(usersRoutes)
 app.register(gymsRoutes)
 app.register(checkInsRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error.',
+      issues: error.flatten().fieldErrors,
+    })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.log(error)
+  } else {
+    // Here we should log to an external tool like (DataDog, NewRelic, Sentry)
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
